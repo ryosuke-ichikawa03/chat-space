de00d2d25261ae9c6931103baa5c36407e6b53a7
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-04-26 21:40:22 +0900

    done_html

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,42 @@
 $(function(){
+  function buildHTML(message){
+    if (message.post && message.image){
+      var html = `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__message-user"> 
+            ${message.user_name}
+          </div>
+          <div class="message-info__message-data">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p>
+            ${message.post}
+          </p>
+          <img src=${message.image} >
+        </div>
+      </div>`
+    } else {
+      var html = `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__message-user"> 
+            ${message.user_name}
+          </div>
+          <div class="message-info__message-data">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p>
+            ${message.post}
+          </p>
+          <img src=${message.image} >
+        </div>
+      </div>`
+    };
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +49,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
+    .done(function(data){
+      var html = buildHTML(data);
+    })
+  }); 
 });
