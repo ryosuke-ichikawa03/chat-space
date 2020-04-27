9c6e4a5a5850b29f611d036351d32f2157dd00c2
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-04-27 22:58:36 +0900

    scroll_message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,41 +1,42 @@
 $(function(){
   function buildHTML(message){
-    if (message.post && message.image){
-      var html = `<div class="message-box">
-        <div class="message-info">
-          <div class="message-info__message-user"> 
+    if (message.image){
+      var html = `<div class="main-chat__group-message__message-box">
+        <div class="main-chat__group-message__message-box__message-info">
+          <div class="main-chat__group-message__message-box__message-info__message-user"> 
             ${message.user_name}
           </div>
-          <div class="message-info__message-data">
+          <div class="main-chat__group-message__message-box__message-info__message-data">
             ${message.created_at}
           </div>
         </div>
-        <div class="message">
+        <div class="main-chat__group-message__message-box__message">
           <p>
             ${message.post}
           </p>
           <img src=${message.image} >
         </div>
       </div>`
+      return html;
     } else {
-      var html = `<div class="message-box">
-        <div class="message-info">
-          <div class="message-info__message-user"> 
+      var html = `<div class="main-chat__group-message__message-box">
+        <div class="main-chat__group-message__message-box__message-info">
+          <div class="main-chat__group-message__message-box__message-info__message-user"> 
             ${message.user_name}
           </div>
-          <div class="message-info__message-data">
+          <div class="main-chat__group-message__message-box__message-info__message-data">
             ${message.created_at}
           </div>
         </div>
-        <div class="message">
+        <div class="main-chat__group-message__message-box__message">
           <p>
             ${message.post}
           </p>
           <img src=${message.image} >
         </div>
       </div>`
-    };
-    return html;
+      return html;
+    }
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -51,6 +52,10 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
+      $('.main-chat__group-message').append(html);
+      $('form')[0].reset();
+      $('.main-chat__group-message').animate({ scrollTop: $('.main-chat__group-message')[0].scrollHeight});
+
     })
-  }); 
-});
+  })
+})
