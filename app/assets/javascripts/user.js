d9bddfa8b29d11909c9f894ac1f912c89b8076ce
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-04-29 19:15:44 +0900

    add-btn_event

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,7 +3,7 @@ $(function(){
     var html =`
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
-        <div class="user-search-add chat-group-user__btn chat--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
       </div>`
     $("#user-search-result").append(html);
   }
@@ -40,4 +40,7 @@ $(function(){
       alert("ユーザーの検索に失敗しました。");
     })
   });
+  $(document).on('click',".chat-group-user__btn--add",function(){
+    console.log("イベント発火")
+  })
 });
