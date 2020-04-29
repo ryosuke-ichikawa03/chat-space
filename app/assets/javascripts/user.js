e8e607bc4dc52650b4b4a76ae9548f64dcafaea7
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-04-29 17:26:53 +0900

    setting_html

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,21 @@
 $(function(){
+  function addUser(user){
+    var html =`
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+      </div>`
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser(){
+    var html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>`
+    $("#user-search-result").append(html);
+  }
+
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -8,10 +25,19 @@ $(function(){
       dataType: 'json'
     })
     .done(function(users){
-      console.log(users);
+      $("#user-search-result").empty()
+      if (users.length !== 0){
+        users.forEach(function(user){
+          addUser(user)
+        })
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function(){
-      console.log('失敗です');
+      alert("ユーザーの検索に失敗しました。");
     })
   });
 });
