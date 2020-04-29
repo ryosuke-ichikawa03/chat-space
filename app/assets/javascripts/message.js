292a7e4edf5163c8d7eedbb4c9f7c7ac8eb3db93
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-04-29 12:18:44 +0900

    repeated_send-btn

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -32,16 +32,18 @@ $(function(){
           <p>
             ${message.post}
           </p>
-          <img src=${message.image} >
         </div>
       </div>`
       return html;
     }
   }
-  $('#new_message').on('submit', function(e){
+  $('#new_message').submit(function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    $('main-chat__message-form__send-btn').removeAttr('data-disable-with')
+//    $(".main-chat__message-form__send-btn").prop('disabled', false);
+//    $(".main-chat__message-form__send-btn").prop('disabled', true);
     $.ajax({
       url: url,
       type: "POST",
@@ -50,12 +52,15 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.main-chat__group-message').append(html);
-      $('form')[0].reset();
-      $('.main-chat__group-message').animate({ scrollTop: $('.main-chat__group-message')[0].scrollHeight});
-
-    })
-  })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.main-chat__group-message').append(html);
+        $('form')[0].reset();
+        $('.main-chat__group-message').animate({ scrollTop: $('.main-chat__group-message')[0].scrollHeight});
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+    });
+    return false;
+  });
 })
