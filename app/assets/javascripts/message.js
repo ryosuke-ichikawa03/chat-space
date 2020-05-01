f714f9915dde84d7f632589b799c7a5da3a883fc
ryosuke-ichikawa03 <ickwrsk@gmail.com>
2020-05-02 02:16:11 +0900

    respons_new-messages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
 $(function(){
+
   function buildHTML(message){
     if (message.image){
-      var html = `<div class="main-chat__group-message__message-box">
+      var html = `<div class="main-chat__group-message__message-box" data-message-id=${message.id}>
         <div class="main-chat__group-message__message-box__message-info">
           <div class="main-chat__group-message__message-box__message-info__message-user"> 
             ${message.user_name}
@@ -19,7 +20,7 @@ $(function(){
       </div>`
       return html;
     } else {
-      var html = `<div class="main-chat__group-message__message-box">
+      var html = `<div class="main-chat__group-message__message-box" data-message-id=${message.id}>
         <div class="main-chat__group-message__message-box__message-info">
           <div class="main-chat__group-message__message-box__message-info__message-user"> 
             ${message.user_name}
@@ -60,4 +61,29 @@ $(function(){
     });
     return false;
   });
+  var reloadMessages = function(){
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML ='';
+        $.each(messages,function(i,message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+        alert('error');
+      
+    })
+  
+  }
+  setInterval(reloadMessages, 7000);
 })
